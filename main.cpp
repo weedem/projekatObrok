e74a5792ddfbc64d4451eb62f8f5ed1d49bd6cfb
weedem <44373654+weedem@users.noreply.github.com>
2019-02-13 17:43:01 +0100

    project done

--- main.cpp
+++ main.cpp
@@ -4,106 +4,82 @@
 #include <cmath>
 #include <time.h>
 #include <cstdlib>
-
-
+#include "header.h"
 using namespace std;
 
-//MESO
-class MESO
-{
+//---------------------------------------
+//      <MESO>
 
-private:
-
-    int gramaza;
-    string vr_mesa;
-
-public:
-            //CONSTRUCTOR 1
-            MESO()
+        //CONSTRUCTOR 1
+        MESO::MESO()
             {
                 gramaza = 0;
                 vr_mesa = "BEZ MESA";
             }
-            //CONSTRUCTOR 2(KOPIJA)
-                MESO(const MESO &m1)
-                {
-                    vr_mesa = m1.vr_mesa;
-                    gramaza = m1.gramaza;
-                }
-            friend bool odabir_meso(int meso,int gramaza);
-            friend void ispisi_obrok();
-            friend string get_meso();
-            friend int get_gramazaO();
-
-            //GET GRAMAZU
-                int get_gramaza()
-                {
-                    return gramaza;
-                }
-            //SET GRAMAZU
-               void set_gramazu(int x)
-                {
-
+        //CONSTRUCTOR 2(KOPIJA)
+        MESO::MESO(const MESO &m1)
+            {
+                vr_mesa = m1.vr_mesa;
+                gramaza = m1.gramaza;
+            }
+        //GET GRAMAZU
+        int MESO::get_gramaza()
+            {
+                return gramaza;
+            }
+        //SET GRAMAZU
+        void MESO::set_gramazu(int x)
+            {
                 gramaza = x;
-                }
-                //GET VRSTU
-                string get_vrmesa()
+            }
+        //GET VRSTU
+        string MESO::get_vrmesa()
+            {
+                return vr_mesa;
+            }
+        //SET VRSTU
+        void MESO::set_vrmesa(int x)
+            {
+                switch(x)
                 {
-                    return vr_mesa;
+                case 1:
+                    vr_mesa = "SVINJETINA";
+                    break;
+                case 2:
+                    vr_mesa = "JUNETINA";
+                    break;
+                case 3:
+                    vr_mesa = "PILETINA";
+                    break;
+                case 4:
+                    vr_mesa = "BEZ_MESA";
                 }
+            }
+        //ISPISI MESO
+        void MESO::ispisi_meso()
+            {
+                cout << vr_mesa << " " << gramaza << endl;
+            }
+
+//    </MESO>
+//---------------------------------------
 
 
-            //SET VRSTU
-                void set_vrmesa(int x)
-                {
-                    switch(x)
-                    {
-                    case 1:
-                        vr_mesa = "SVINJETINA";
-                        break;
-                    case 2:
-                        vr_mesa = "JUNETINA";
-                        break;
-                    case 3:
-                        vr_mesa = "PILETINA";
-                        break;
-                    case 4:
-                        vr_mesa = "BEZ_MESA";
+//---------------------------------------
+//      <PRILOG>
 
-                    }
-        }
-                    void ispisi_meso()
-                    {
-                    cout << vr_mesa << " " << gramaza << endl;
-                    }
-
-
-
-};
-    //PRILOG
-class PRILOG
-{
-private:
-    string prilog;
-public:
     //CONSTRUCTOR 1
-            PRILOG()
-                    {
-                    prilog = "BEZ_PRILOGA";
-
-                    }
+        PRILOG::PRILOG()
+            {
+                prilog = "BEZ_PRILOGA";
+            }
     //CONSTRUCTOR 2(KOPIJA)
-            PRILOG(const PRILOG &p1)
-
-                    {
+        PRILOG::PRILOG(const PRILOG &p1)
+            {
                 prilog = p1.prilog;
-                    }
-    friend bool odabir_prilog(int prilog);
-    friend void ispisi_obrok();
-    friend string get_prilog();
-
+            }
     //SET PRILOG
-            void set_prilog(int x)
+        void PRILOG::set_prilog(int x)
             {
             switch(x)
                 {
@@ -118,120 +94,117 @@ public:
                 break;
             case 4:
                 prilog = "TESTENINA";
+                break;
             case 5:
                 prilog = "BEZ_PRILOGA";
-
+                break;
                 }
             }
     //GET PRILOG
-
-            string get_prilog()
+        string PRILOG::get_prilog()
             {
                 return prilog;
             }
-    void ispisi_prilog()
+    //ISPISI PRILOG
+        void PRILOG::ispisi_prilog()
             {
-
                 cout << prilog << endl;
             }
+//    </PRILOG>
+//---------------------------------------
 
-};
-    //OBROK
-class OBROK
-{
+//---------------------------------------
+//      <OBROK>
 
-
-private:
-        MESO m;
-        PRILOG p;
-        string obrok;
-public:
     //CONSTRUCTOR 1
-
-    OBROK()
+        OBROK::OBROK()
             {
                 obrok = "Pocetak_odabira";
             }
     //CONSTRUCTOR SA PARAMETRIMA
-
-    OBROK(string x)
-    {
-        obrok = x;
-    }
-
-        //GET PRILOG
-
-            string get_prilog()
+        OBROK::OBROK(string x)
+            {
+                obrok = x;
+            }
+    //GET PRILOG
+       string OBROK::get_prilog()
             {
                 return p.get_prilog();
             }
-        //GET MESO
-
-            string get_meso()
+    //GET MESO
+        string OBROK::get_meso()
             {
                 return m.get_vrmesa();
             }
-        //GET GRAMAZA
-
-            int get_gramazaO()
+    //GET GRAMAZA
+        int OBROK::get_gramazaO()
             {
                 return m.get_gramaza();
             }
-        //GET STANJE
-
-            string get_stanje()
+    //GET STANJE
+        string OBROK::get_stanje()
             {
                 return obrok;
             }
-        //ODABIR MESA
-
-        bool odabir_meso(int meso,int gramaza)
-        {
-        m.set_vrmesa(meso);
-        m.set_gramazu(gramaza);
-        if(gramaza < 0 ||  gramaza > 1000)
-            return false;
-        else
-        {
-
-            obrok  = "Odabrano_Meso";
-            return true;
-        }
-        }
-        //ODABIR PRILOGA
-        bool odabir_prilog(int prilog)
-                {
-                p.set_prilog(prilog);
-                if(prilog == 4 && obrok == "Pocetak_odabira" )
+    //ODABIR MESA
+        bool OBROK::odabir_meso(int meso,int gramaza)
+            {
+                m.set_vrmesa(meso);
+                m.set_gramazu(gramaza);
+                if(gramaza < 0 ||  gramaza > 1000)
                     return false;
-                else{
+                else
+                {
+                    obrok  = "Odabrano_Meso";
+                    return true;
+                }
+            }
+    //ODABIR PRILOGA
+        bool OBROK::odabir_prilog(int prilog)
+            {
+                p.set_prilog(prilog);
+                if(obrok == "Pocetak_odabira" && prilog == 5)
+                    return false;
+                else
+                    {
                     obrok = "Odabrano_Prilog";
                     return true;
                     }
-                }
-        //SPREMI
-        void spremi()
-        {
-            srand (time(NULL));
-            if((rand() % 100) > 90)
-                obrok = "ZAGORE";
-            else
-            {
-                obrok = "SPREMAN";
             }
-        }
-        //ISPISI OBROK
-        void ispisi_obrok()
+    //SPREMI
+        void OBROK::spremi()
+            {
+                srand (time(NULL));
+                if((rand() % 100) > 90)
+                    obrok = "VAS OBROK JE ZAGOREO";
+                else
+                {
+                    obrok = "VAS OBROK JE SPREMAN";
+                }
+            }
+    //ISPISI OBROK
+        void OBROK::ispisi_obrok()
         {
             string meso = m.get_vrmesa();
             int gramaza = m.get_gramaza();
             string prilog = p.get_prilog();
-            cout << obrok <<":" << endl;
             cout << meso << " " << gramaza << "gr " << prilog<< endl;
         }
-};
-void meni();
 
+
+//    </OBROK>
+//---------------------------------------
+        void meni()
+        {
+            cout << "================= MEKSICKA =================" << endl ;
+            cout << "           ===  1. ODABIR MESA  ===          " << endl;
+            cout << "           ===  2. ODABIR PRILOGA  ===          " << endl;
+            cout << "           ===  3. SPREMI OBROK  ===          " << endl;
+            cout << "           ===  4. ISPISI OBROK  ===          " << endl;
+            cout << "           ===  5. ISPISI STANJE  ===          " << endl;
+            cout << "           ===  6. EXIT  ===          " << endl;
+            cout << "===========================================" << endl;
+        }
 int main()
 {
     OBROK o;
@@ -295,15 +268,4 @@ int main()
     }
 
 }
-void meni()
-{
-    cout << "================= MEKSICKA =================" << endl << endl;
-    cout << "           ===  1. ODABIR MESA  ===          " << endl;
-    cout << "           ===  2. ODABIR PRILOGA  ===          " << endl;
-    cout << "           ===  3. SPREMI OBROK  ===          " << endl;
-    cout << "           ===  4. ISPISI OBROK  ===          " << endl;
-    cout << "           ===  5. ISPISI STANJE  ===          " << endl;
-    cout << "           ===  6. EXIT  ===          " << endl;
-    cout << "===========================================" << endl;
 
-}
